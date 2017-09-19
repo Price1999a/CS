84e5ae91d71b9c791edd8c7262d7269d06a5e9ed
Shen Tianqi <ShenTianqi@users.noreply.github.com>
2017-09-19 21:24:22 +0800

    5
    5

--- 源.c
+++ 源.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 int main()
 {
 	int fahr;
 
-	for (fahr = 300;
-		fahr >= 0;
+	for (fahr = UPPER;
+		fahr >= LOWER;
 		fahr = fahr - 20)
 		printf("%3d\t%6.1f\n", fahr, (5.0 / 9.0)*(fahr - 32));
 		system("pause");
+		return 0;
 }
