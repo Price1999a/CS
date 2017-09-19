608b0e03d276f4d0f8ed1d37cd4193d7c15db487
Shen Tianqi <ShenTianqi@users.noreply.github.com>
2017-09-19 21:23:00 +0800

    2
    2

--- 源.c
+++ 源.c
@@ -2,6 +2,17 @@
 
 int main()
 {
-	printf("hello,world\n");
+	int fahr, celsius, lower, upper, step;
+
+	lower = 0;
+	upper = 300;
+	step = 1;
+
+	fahr = lower;
+	while (fahr <= upper) {
+		celsius = 5 * (fahr - 32) / 9;
+		printf("%d\t%d\n", fahr, celsius);
+		fahr = fahr + step;
+	}
 	system("pause");
 }
